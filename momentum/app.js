d38bb3b8aeb0db4f58481eb50f063bfd018e9514
woogi <dbwhddnr00@naver.com>
2021-10-03 11:22:24 +0900

    const, let

--- momentum/app.js
+++ momentum/app.js
@@ -1,8 +1,12 @@
-const a = 10;
-const b = 2;
+let a = 10;
+let b = 2;
 const myName = "woogi";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello " + myName);
+
+myName = "woosi"
+
+console.log("your new name is " + myName);
