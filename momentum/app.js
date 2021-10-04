198c8f8840b56e4dd0f29c34f7c813f201f4611e
woogi <dbwhddnr00@naver.com>
2021-10-04 11:20:40 +0900

    null undefined

--- momentum/app.js
+++ momentum/app.js
@@ -1,12 +1,3 @@
-let a = 10;
-let b = 2;
-const myName = "woogi";
-
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName);
-
-myName = "woosi"
-
-console.log("your new name is " + myName);
+const amIFat = null;
+let something;
+console.log(something, amIFat);
