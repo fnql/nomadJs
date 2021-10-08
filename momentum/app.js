2c9b6c6e9307a303088bf5f1f444c7cdb6172ab9
fnql <dbwhddnr00@naver.com>
2021-10-08 12:58:43 +0900

    recap

--- momentum/app.js
+++ momentum/app.js
@@ -1,9 +1,23 @@
-const player = {
-    name: "woogi",
-    sayHello: function(a){
-        console.log("hello! " + a +" nice");
+const calculator = {
+    add: function(a,b) {
+        console.log(a+b);
+    },
+    minus: function(a,b) {
+        console.log(a-b);
+    },
+    div: function(a,b) {
+        console.log(a/b);
+    },
+    mul: function(a,b) {
+        console.log(a*b);
+    },
+    power: function(a,b) {
+        console.log(a**b);
     }
 }
 
-console.log(player.name);
-player.sayHello("lynn");
+calculator.add(4,5);
+calculator.minus(10,2);
+calculator.div(50,5);
+calculator.mul(7,3);
+calculator.power(3,5);
