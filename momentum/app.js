fa3e87a5e76902a4b19beb6d65ffeb0cfe588bd7
woogi <dbwhddnr00@naver.com>
2021-10-09 10:50:38 +0900

    func return

--- momentum/app.js
+++ momentum/app.js
@@ -1,23 +1,8 @@
-const calculator = {
-    add: function(a,b) {
-        console.log(a+b);
-    },
-    minus: function(a,b) {
-        console.log(a-b);
-    },
-    div: function(a,b) {
-        console.log(a/b);
-    },
-    mul: function(a,b) {
-        console.log(a*b);
-    },
-    power: function(a,b) {
-        console.log(a**b);
-    }
+const age = 96;
+function calAge(agei){
+    return agei +2;
 }
 
-calculator.add(4,5);
-calculator.minus(10,2);
-calculator.div(50,5);
-calculator.mul(7,3);
-calculator.power(3,5);
+const krAge = calAge(age);
+
+console.log(krAge);
