6a36d683d019be705eaa000fa54660b6478e6eec
fnql <dbwhddnr00@naver.com>
2021-10-15 11:26:06 +0900

    mouse, window event

--- momentum/app.js
+++ momentum/app.js
@@ -1,7 +1,38 @@
 const title = document.querySelector(".hello h1");
 
-function handleTitle(){
+console.dir(title);
+function handleTitleClick(){
     title.style.color= "blue";
 }
 
-title.addEventListener("click",handleTitle);
+function handleMouseEnter(){
+    title.innerText = "Mouse is here!!";
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is Gone";
+}
+
+function handleWindowResize(){
+    document.body.style.backgroundColor  = "tomato";
+}
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+
+function handleWindowOffline(){
+    alert("SOS No wifi");
+}
+
+function handleWindowOnline(){
+    alert("i'm Online, Good");
+}
+title.onclick = handleTitleClick;
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave);
+
+window.addEventListener("resize",handleWindowResize);
+window.addEventListener("copy",handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
