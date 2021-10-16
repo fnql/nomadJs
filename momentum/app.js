8047b3b86a73299f1b107b6231b0133c3f4dea91
woogi <dbwhddnr00@naver.com>
2021-10-16 13:07:45 +0900

    togle, css

--- momentum/app.js
+++ momentum/app.js
@@ -1,38 +1,7 @@
-const title = document.querySelector(".hello h1");
+const h1 = document.querySelector(".hello h1");
 
-console.dir(title);
 function handleTitleClick(){
-    title.style.color= "blue";
+    h1.classList.toggle("clicked");
 }
 
-function handleMouseEnter(){
-    title.innerText = "Mouse is here!!";
-}
-
-function handleMouseLeave(){
-    title.innerText = "Mouse is Gone";
-}
-
-function handleWindowResize(){
-    document.body.style.backgroundColor  = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS No wifi");
-}
-
-function handleWindowOnline(){
-    alert("i'm Online, Good");
-}
-title.onclick = handleTitleClick;
-title.addEventListener("mouseenter",handleMouseEnter);
-title.addEventListener("mouseleave",handleMouseLeave);
-
-window.addEventListener("resize",handleWindowResize);
-window.addEventListener("copy",handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+h1.addEventListener("click",  handleTitleClick);
