308b87c7f051d342ef3e92d44b6599e768a48eff
fnql <dbwhddnr00@naver.com>
2021-10-29 09:09:12 +0900

    string to json

--- momentum/js/todo.js
+++ momentum/js/todo.js
@@ -2,10 +2,11 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
+const TODOS_KEY = "todos";
 const toDos = [];
 
 function saveToDos(){
-    localStorage.setItem("toDos",JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY,JSON.stringify(toDos));
 }
 
 function deleteToDo(event){
@@ -36,3 +37,10 @@ function handleToDoSubmit(event){
 }
 
 toDoForm.addEventListener("submit",handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos){
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach((item) => console.log("is", item));
+}
