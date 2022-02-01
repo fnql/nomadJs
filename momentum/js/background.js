a54556083a8f32f87260f423466355a3ce15197d
woogi <dbwhddnr00@naver.com>
2022-02-01 16:29:00 +0900

    css 대신 js로 이미지 설정하는법

--- momentum/js/background.js
+++ momentum/js/background.js
@@ -5,7 +5,7 @@ const images = [
 ];
 const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
-
+//style 추가  하는법  
 bgImage.src=`img/${chosenImage}`;
 
 document.body.appendChild(bgImage);
